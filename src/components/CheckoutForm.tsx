6c5d4f1875d9a3011e7498f1de81283f6ba3fa0a
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-10 13:36:18 +0200

    add checkout form

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useLoading } from "../hooks/useLoading";
 
 export default function CheckoutForm() {
     const stripe = useStripe();
     const elements = useElements();
 
     const [message, setMessage] = useState<string | undefined>("");
-    const [isLoading, setIsLoading] = useState(false);
+    const { loading, setLoading } = useLoading();
 
     useEffect(() => {
         if (!stripe) {
@@ -42,35 +43,26 @@ export default function CheckoutForm() {
 
     const handleSubmit = async (e: React.SyntheticEvent) => {
         e.preventDefault();
-
         if (!stripe || !elements) {
-            // Stripe.js hasn't yet loaded.
-            // Make sure to disable form submission until Stripe.js has loaded.
             return;
         }
-
-        setIsLoading(true);
+        setLoading(true);
 
         const { error } = await stripe.confirmPayment({
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: "http://localhost:3000",
+                return_url: "http://localhost:5173",
             },
         });
 
-        // This point will only be reached if there is an immediate error when
-        // confirming the payment. Otherwise, your customer will be redirected to
-        // your `return_url`. For some payment methods like iDEAL, your customer will
-        // be redirected to an intermediate site first to authorize the payment, then
-        // redirected to the `return_url`.
         if (error && (error.type === "card_error" || error.type === "validation_error")) {
             setMessage(error.message);
         } else {
             setMessage("An unexpected error occurred.");
         }
 
-        setIsLoading(false);
+        setLoading(false);
     };
 
     const paymentElementOptions = {
@@ -82,7 +74,7 @@ export default function CheckoutForm() {
             <PaymentElement id="payment-element" options={paymentElementOptions} />
             <div style={{ display: "flex", justifyContent: "center" }}>
                 <button
-                    disabled={isLoading || !stripe || !elements}
+                    disabled={loading || !stripe || !elements}
                     id="submit"
                     style={{
                         marginTop: "20px",
@@ -92,7 +84,7 @@ export default function CheckoutForm() {
                     }}
                 >
                     <span id="button-text">
-                        {isLoading ? (
+                        {loading ? (
                             <div className="spinner" id="spinner">
                                 Waiting
                             </div>
