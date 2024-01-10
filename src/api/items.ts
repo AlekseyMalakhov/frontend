a1f6cb0ebf4483c826cd6294ff46323dc1717cb8
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-10 14:28:25 +0200

    some more changes 2

--- src/api/items.ts
+++ src/api/items.ts
@@ -1,3 +1,4 @@
+import { Item } from "../types/item";
 import client from "./client";
 
 const getItems = () => {
@@ -9,8 +10,18 @@ const getItems = () => {
         .catch((error) => error.response);
 };
 
+const createPaymentIntent = (data: Item) => {
+    return client
+        .post(`/payment-intent`, data)
+        .then((response) => {
+            return response;
+        })
+        .catch((error) => error.response);
+};
+
 const itemsAPI = {
     getItems,
+    createPaymentIntent,
 };
 
 export default itemsAPI;
--- src/views/Payment.tsx
+++ src/views/Payment.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../components/CheckoutForm";
+import itemsAPI from "../api/items";
+import { useLoading } from "../hooks/useLoading";
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
@@ -16,16 +18,35 @@ const appearance = {
 export default function Payment() {
     const itemToBuy = useAppSelector((state) => state.manage.itemToBuy);
     const [clientSecret, setClientSecret] = useState("");
+    const { setLoading } = useLoading();
 
     useEffect(() => {
         // Create PaymentIntent as soon as the page loads
-        fetch("/create-payment-intent", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ items: [{ id: "xl-tshirt34" }] }),
-        })
-            .then((res) => res.json())
-            .then((data) => setClientSecret(data.clientSecret));
+        // fetch("/create-payment-intent", {
+        //     method: "POST",
+        //     headers: { "Content-Type": "application/json" },
+        //     body: JSON.stringify({ items: [{ id: "xl-tshirt34" }] }),
+        // })
+        //     .then((res) => res.json())
+        //     .then((data) => setClientSecret(data.clientSecret));
+        if (!itemToBuy) {
+            return;
+        }
+        setLoading(true);
+        itemsAPI
+            .createPaymentIntent(itemToBuy)
+            .then((response) => {
+                if (response?.status === 200) {
+                    setClientSecret(response.data);
+                } else {
+                    console.log("Error on creating payment intent");
+                }
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false);
+            });
     }, []);
 
     if (!itemToBuy) {
