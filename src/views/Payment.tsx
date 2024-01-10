23f96834597fa170e93197f7a85359942d853f22
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-10 13:29:08 +0200

    show what you are paying for

--- src/views/Payment.tsx
+++ src/views/Payment.tsx
@@ -1,3 +1,15 @@
+import { useAppSelector } from "../hooks/reduxHooks";
+
 export default function Payment() {
-    return <div>Payment</div>;
+    const itemToBuy = useAppSelector((state) => state.manage.itemToBuy);
+
+    if (!itemToBuy) {
+        return "No item to buy";
+    }
+
+    return (
+        <div>
+            Payment for {itemToBuy.name}. Price {itemToBuy.price}$
+        </div>
+    );
 }
