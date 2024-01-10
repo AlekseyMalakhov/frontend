82c22958ff7f82d0e7720069edb73a1903e8ab77
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-10 08:15:15 +0200

    add useItems

---
+++ src/hooks/useItems.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import itemsAPI from "../api/fields";
+
+export const useItems = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        if (!items) {
+            itemsAPI
+                .getItems()
+                .then((response) => {
+                    if (response?.status === 200) {
+                        setItems(response.data);
+                    } else {
+                        console.log("Error on getting items");
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, []);
+};
