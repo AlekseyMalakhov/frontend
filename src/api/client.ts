c1dc29aa762535ef2aaaa08077fa97f0ca581c98
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-11 13:01:52 +0200

    small changes

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "http://localhost:3000",
+    //baseURL: "http://localhost:3000",
+    baseURL: "https://6487-188-69-11-194.ngrok-free.app",
+    headers: {
+        "ngrok-skip-browser-warning": "69420",
+    },
 });
 
 export default client;
