c01601dc2d7879644c024f2448663cbc8b68ca44
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-16 10:25:12 +0200

    use env var

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const client = axios.create({
     //baseURL: "http://localhost:3000",
-    baseURL: "https://6487-188-69-11-194.ngrok-free.app",
+    baseURL: import.meta.env.VITE_BASE_URL,
     headers: {
         "ngrok-skip-browser-warning": "69420",
     },
