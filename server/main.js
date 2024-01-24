5c2a487240928be7f886d96f5f301f9c06f3df05
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-24 20:37:04 +0200

    rename to dist

--- server/main.js
+++ server/main.js
@@ -7,10 +7,10 @@ const app = express();
 const port = process.env.PORT || 3010;
 app.use(cors());
 const dirName = getDirName(import.meta.url);
-app.use("/static", express.static(path.join(dirName, "build")));
+app.use("/static", express.static(path.join(dirName, "dist")));
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(dirName, "/build/index.html"));
+    res.sendFile(path.join(dirName, "/dist/index.html"));
 });
 
 //start the server
